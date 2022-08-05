228b8461026f1a0ad3b876cd8b6ff4e9145d1358
su-jin02 <sujinjeon587@gmail.com>
2022-08-05 23:42:36 +0900

    20220805

--- TheFoodProject/TheFood/src/main/java/TheFoodProject/TheFood/entity/Role.java
+++ TheFoodProject/TheFood/src/main/java/TheFoodProject/TheFood/entity/Role.java
@@ -1,22 +1,23 @@
 package TheFoodProject.TheFood.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-    private Integer id;
+    private Long id;
 
     private String name;
 
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
-
-
 }
--- TheFoodProject/TheFood/src/main/java/TheFoodProject/TheFood/entity/User.java
+++ TheFoodProject/TheFood/src/main/java/TheFoodProject/TheFood/entity/User.java
@@ -33,4 +33,5 @@ public class User {
     private List<Role> roles = new ArrayList<>();
 
 
+
 }
