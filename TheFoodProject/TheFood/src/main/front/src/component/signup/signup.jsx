383f8209ee72fb487c5a3c8bcf7add202de0784c
rewritestar <rewritestar@naver.com>
2022-08-31 18:12:35 +0900

    로그인 회원가입 axios 추가

--- TheFoodProject/TheFood/src/main/front/src/component/signup/signup.jsx
+++ TheFoodProject/TheFood/src/main/front/src/component/signup/signup.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Container from "../container/container";
