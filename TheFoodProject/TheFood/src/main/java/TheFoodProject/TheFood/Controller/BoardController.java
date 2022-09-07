0ecc1aba3356473c82ec7ffe6a769c25ca48c1ef
su-jin02 <sujinjeon587@gmail.com>
2022-09-07 22:19:34 +0900

    20220907벡엔드수정

--- TheFoodProject/TheFood/src/main/java/TheFoodProject/TheFood/Controller/BoardController.java
+++ TheFoodProject/TheFood/src/main/java/TheFoodProject/TheFood/Controller/BoardController.java
@@ -5,10 +5,8 @@ import TheFoodProject.TheFood.service.BoardService;
 import TheFoodProject.TheFood.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -32,53 +30,52 @@ public class BoardController {
     }
 
     @PostMapping("/api/recipe")
-    public List<Board> recipeBoardList(Integer category){
+    public List<Board> recipeBoardList(@RequestBody Integer category){
         return boardService.boardList1(category);
     }
     @PostMapping("/api/restaurant")
-    public List<Board> restaurantBoardList(Integer category){
+    public List<Board> restaurantBoardList(@RequestBody Integer category){
         return boardService.boardList1(category);
     }
     @PostMapping("/api/free")
-    public List<Board> freeBoardList(Integer category){
+    public List<Board> freeBoardList(@RequestBody Integer category){
         return boardService.boardList1(category);
     }
 
 
-    @GetMapping("/board/view") // localhost:8080/board/view?id=1
-    public String boardView(Model model, Integer id){
-
-        model.addAttribute("board", boardService.boardView(id));
-        model.addAttribute("list1", commentService.commentList(id));
-        return "boardview";
-    }
+//    @GetMapping("/board/view") // localhost:8080/board/view?id=1
+//    public String boardView(Model model, Integer id){
+//
+//        model.addAttribute("board", boardService.boardView(id));
+//        model.addAttribute("list1", commentService.commentList(id));
+//        return "boardview";
+//    }
 
     @PostMapping("/api/boarddelete")
-    public void boardDelete(Integer id){
-
+    public void boardDelete(@RequestBody Integer id){
         boardService.boardDelete(id);
     }
 
 
-    @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable("id") Integer id, Model model) {
-
-        model.addAttribute("board", boardService.boardView(id));
-
-        return "boardmodify";
-    }
-
-    @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file, Authentication authentication) throws Exception{
-
-        Board boardTemp = boardService.boardView(id);
-        boardTemp.setTitle(board.getTitle());
-        boardTemp.setContent(board.getContent());
-
-        String username = authentication.getName();
-        boardService.write(username, boardTemp, file);
-
-        return "redirect:/board/list";
-    }
+//    @GetMapping("/board/modify/{id}")
+//    public String boardModify(@PathVariable("id") Integer id, Model model) {
+//
+//        model.addAttribute("board", boardService.boardView(id));
+//
+//        return "boardmodify";
+//    }
+//
+//    @PostMapping("/board/update/{id}")
+//    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file, Authentication authentication) throws Exception{
+//
+//        Board boardTemp = boardService.boardView(id);
+//        boardTemp.setTitle(board.getTitle());
+//        boardTemp.setContent(board.getContent());
+//
+//        String username = authentication.getName();
+//        boardService.write(username, boardTemp, file);
+//
+//        return "redirect:/board/list";
+//    }
 
 }
--- TheFoodProject/TheFood/src/main/java/TheFoodProject/TheFood/Controller/UserController.java
+++ TheFoodProject/TheFood/src/main/java/TheFoodProject/TheFood/Controller/UserController.java
@@ -1,13 +1,12 @@
 package TheFoodProject.TheFood.Controller;
 
 import TheFoodProject.TheFood.entity.LoginForm;
+import TheFoodProject.TheFood.entity.MypageForm;
 import TheFoodProject.TheFood.entity.SignupForm;
 import TheFoodProject.TheFood.entity.User;
 import TheFoodProject.TheFood.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +18,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-//    @PostMapping("/api/signup")
-//    public User usersignupForm(String userid, String userpassword, String useremail, String username){
-//        User newUser = new User();
-//        newUser.setUserid(userid);
-//        newUser.setUserpassword(userpassword);
-//        newUser.setUseremail(useremail);
-//        newUser.setUsername(username);
-//        return userService.save(newUser);
-//    }
-
-
+    //회원가입
     @PostMapping("/api/signup")
     public User usersignupForm(@RequestBody SignupForm signupForm){
         User newUser = new User();
@@ -42,7 +31,8 @@ public class UserController {
 //--------------------------------------------------------------------------------------------------
     //아이디,비밀번호 찾기
     @PostMapping("api/findid")
-    public String userFindId(String useremail){
+    public String userFindId(@RequestBody String useremail){
+
         return userService.findid(useremail);
     }
 
@@ -75,22 +65,6 @@ public class UserController {
 
 //--------------------------------------------------------------------------------------------------
     //로그인
-//    @PostMapping("/api/login")
-//    public LoginForm userloginForm(@RequestBody LoginForm loginForm){
-//        System.out.println("로그인 axios 연결 성공" + loginForm);
-//        return loginForm;
-//    }
-
-//    @PostMapping("/api/signup")
-//    public User usersignupForm(@RequestBody SignupForm signupForm){
-//        User newUser = new User();
-//        newUser.setUserid(signupForm.getUserid());
-//        newUser.setUserpassword(signupForm.getUserpassword());
-//        newUser.setUseremail(signupForm.getUseremail());
-//        newUser.setUsername(signupForm.getUsername());
-//        return userService.save(newUser);
-//    }
-
     @PostMapping("api/login")
     public User userLogin(@RequestBody LoginForm loginForm){
 
@@ -100,7 +74,8 @@ public class UserController {
 //--------------------------------------------------------------------------------------------------
 //탈퇴
     @PostMapping("/api/withdrawal/")
-    public void delete(Integer id){
+    public void delete(@RequestBody Integer id){
+
         userService.delete(id);
     }
 
@@ -108,27 +83,24 @@ public class UserController {
     //마이페이지
 
     @PostMapping("/api/mypage")
-    public User mypage(String username, String userpassword){
-        return userService.mypage(username, userpassword);
+    public User mypage(@RequestBody MypageForm mypageForm){
+
+        return userService.mypage(mypageForm.getUsername(), mypageForm.getUserpassword());
     }
 
-    @GetMapping("/user/modify")
-    public String userModify(){
-        return "usermodify";
-    }
-
-    @PostMapping("/user/modifypro")
-    public String userUpdate(User user, Authentication authentication) throws Exception{
-
-        String userid = authentication.getName();
-        userService.modify(user, userid);
-
-        return "redirect:/board/list";
-    }
+//    @PostMapping("/user/modifypro")
+//    public String userUpdate(User user, Authentication authentication) throws Exception{
+//
+//        String userid = authentication.getName();
+//        userService.modify(user, userid);
+//
+//        return "redirect:/board/list";
+//    }
 //-----------------------------------------------------
     //stay login
     @PostMapping("/api/staylogin/")
-    public User userStay(Integer id){
+    public User userStay(@RequestBody Integer id){
+
         return userService.stay(id);
     }
 }
