c7322c4da5e4cc90fd2c5b91f305e418d3faa898
rewritestar <rewritestar@naver.com>
2022-09-30 12:05:08 +0900

    반응형 헤더 ok

--- TheFoodProject/TheFood/src/main/front/src/component/header/header.jsx
+++ TheFoodProject/TheFood/src/main/front/src/component/header/header.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./header.module.css";
 import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
 const Header = () => {
+  const [isActive, setActive] = useState(false);
+
   const navigate = useNavigate();
   const goToHome = () => {
     navigate("/");
@@ -22,6 +25,9 @@ const Header = () => {
   const goToAbout = (e) => {
     navigate("/aboutus");
   };
+  const styles_res = isActive ? styles.active : styles.none;
+  console.log(isActive);
+
   return (
     <div className={styles.container}>
       <section className={styles.logo} onClick={goToHome}>
@@ -32,30 +38,37 @@ const Header = () => {
         ></img>
         <h1 className={styles.title}>The Food</h1>
       </section>
-      <section className={styles.category}>
-        <p className={styles.name}>-category-</p>
-        <div className={styles.buttons}>
-          <button className={styles.button} onClick={goToRecipe}>
-            레시피
-          </button>
-          <button className={styles.button} onClick={goToRestaurant}>
-            맛집
-          </button>
-          <button className={styles.button} onClick={goToFree}>
-            일상
-          </button>
-        </div>
-      </section>
-      <button className={styles.about_btn} onClick={goToAbout}>
-        About us
-      </button>
-      <section className={styles.write}>
-        <p className={styles.name}>-write-</p>
-        <button className={styles.write_btn} onClick={goToBoardWrite}>
-          게시글 작성
+      <section className={`${styles.menu} ${styles_res}`}>
+        <section className={styles.category}>
+          <p className={styles.name}>-category-</p>
+          <div className={styles.buttons}>
+            <button className={styles.button} onClick={goToRecipe}>
+              레시피
+            </button>
+            <button className={styles.button} onClick={goToRestaurant}>
+              맛집
+            </button>
+            <button className={styles.button} onClick={goToFree}>
+              일상
+            </button>
+          </div>
+        </section>
+        <button className={styles.about_btn} onClick={goToAbout}>
+          About us
         </button>
+        <section className={styles.write}>
+          <p className={styles.name}>-write-</p>
+          <button className={styles.write_btn} onClick={goToBoardWrite}>
+            게시글 작성
+          </button>
+        </section>
       </section>
-      <AiOutlineMenu className={styles.res_btn} />
+      <AiOutlineMenu
+        className={styles.res_btn}
+        onClick={() => {
+          setActive(!isActive);
+        }}
+      />
     </div>
   );
 };
